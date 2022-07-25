e668d8fc339f0741372a0e2cdb44a87fa402f5fe
Senior-Pomidor <bobylev.vladi@gmail.com>
2022-07-25 18:22:07 +0300

    init and created base components structure

---
+++ src/components/Branch.vue
@@ -0,0 +1,15 @@
+<template>
+	<li>
+		Branch
+	</li>
+</template>
+
+<script>
+	export default {
+		name: 'branch'
+	}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Tree.vue
@@ -0,0 +1,80 @@
+<template>
+	<section class="section-tree">
+		<h2>
+			Бинарная структура
+		</h2>
+		
+		<ul class="tree">
+			<Branch v-for="branch in tree" :key="branch.id"
+					:class="{folder: branch.type === 'folder', file: branch.type === 'file'}"
+					>
+					<!-- {{ branch.type }} -->
+			</Branch>
+		</ul>
+	</section>
+</template>
+
+<script>
+	import Branch from '@/components/Branch'
+	export default {
+		name: 'tree',
+		components: {
+			Branch
+		},
+		data() {
+			return {
+				tree: [
+					{
+						id: 1,
+						type: 'folder',
+						children: [
+							{
+								id: 2,
+								type: 'file'
+							}
+						]
+					},
+					{
+						id: 3,
+						type: 'folder',
+						children: [
+							{
+								id: 4,
+								type: 'folder',
+								children: [
+									{
+										id: 4,
+										type: 'file'
+									}, 
+									{
+										id: 4,
+										type: 'file'
+									}
+								]
+							}, 
+							{
+									id: 5,
+									type: 'file'
+							}
+						]
+					},
+					{
+						id: 6,
+						type: 'folder',
+						children: []
+					},
+					{
+						id: 7,
+						type: 'file'
+					}
+				]
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.folder {
+		color: red;
+	}
+</style>
