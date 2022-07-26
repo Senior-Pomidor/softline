471416b648774ff6c431d9dee19881c1f7492829
Senior-Pomidor <bobylev.vladi@gmail.com>
2022-07-26 10:57:42 +0300

    buildd

---
+++ precache-manifest.5fe53da633b6ca3bcfbe608fab752c16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84a9829853499625950",
+    "url": "/softline/css/app.f40f2636.css"
+  },
+  {
+    "revision": "07894ef34e8462b77b24",
+    "url": "/softline/css/chunk-0ca3c3fd.6d4b8b68.css"
+  },
+  {
+    "revision": "afa1f43b7d889c36257846194710e9c1",
+    "url": "/softline/index.html"
+  },
+  {
+    "revision": "c84a9829853499625950",
+    "url": "/softline/js/app.60bd6eb5.js"
+  },
+  {
+    "revision": "07894ef34e8462b77b24",
+    "url": "/softline/js/chunk-0ca3c3fd.70038523.js"
+  },
+  {
+    "revision": "0a987fc6f4463400acb2",
+    "url": "/softline/js/chunk-vendors.40c958b2.js"
+  },
+  {
+    "revision": "b0bd5e1baf276d869d530f7a3dbb5b5e",
+    "url": "/softline/manifest.json"
+  }
+]);
