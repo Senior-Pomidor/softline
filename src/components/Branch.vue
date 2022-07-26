06b256703776f7170c7464aa8194028522b5a232
Senior-Pomidor <bobylev.vladi@gmail.com>
2022-07-26 12:17:35 +0300

    refactor

--- src/components/Branch.vue
+++ src/components/Branch.vue
@@ -1,190 +1,57 @@
 <template>
-	<div ref="branch" class="branch" :class="'branch--' + info.type">
-		<button class="branch__btn" v-if="info.children && info.children.length > 0" @click.stop="toggleOpenFolder()" :class="{ 'opened': isSubtreeOpened}">
+	<li ref="branch" class="tree__branch" :class="'tree__branch--' + info.type">
+		<button class="tree__btn" v-if="info.children && info.children.length > 0" @click.stop="toggleOpenFolder()"
+			:class="{ 'opened': isSubtreeOpened }">
 			<!-- + -->
 		</button>
-		
-		<p class="branch__name">
+
+		<span class="tree__branch-name">
 			{{ level }} {{ info.type }} {{ info.name }}
-		</p>
-		
-		
+		</span>
+
 		<template v-if="info.type === 'folder' && info.children.length > 0">
-			<div class="branch__subtree" v-if="isSubtreeOpened">
+			<ul class="tree__subtree" v-if="isSubtreeOpened">
 				<branch v-for="child in info.children" :info="child" :level="level + 1" :key="child.id" />
-			</div>
+			</ul>
 		</template>
-	</div>
+	</li>
 </template>
 
 <script>
-	export default {
-		name: 'branch',
-		props: {
-			info: {
-				type: Object,
-				default() {
-					return {
-						id: null,
-						type: null,
-						children: [],
-					};
-				}
+export default {
+	name: 'branch',
+	props: {
+		info: {
+			type: Object,
+			default() {
+				return {
+					id: null,
+					type: null,
+					children: [],
+				};
+			}
+		},
+		level: {
+			type: Number,
+			default() {
+				return 0;
 			},
-			level: {
-				type: Number,
-				default() {
-					return 0;
-				},
-			}
-		},
-		data() {
-			return {
-				isSubtreeOpened: false
-			}
-		},
-		methods: {
-			toggleOpenFolder() {
-				this.$refs.branch.classList.toggle('opened')
-				this.isSubtreeOpened = !this.isSubtreeOpened
-			}
+		}
+	},
+	data() {
+		return {
+			isSubtreeOpened: false
+		}
+	},
+	methods: {
+		toggleOpenFolder() {
+			this.$refs.branch.classList.toggle('opened')
+			this.isSubtreeOpened = !this.isSubtreeOpened
 		}
 	}
+}
 </script>
 
 <style lang="scss">
-	.tree {
-		--line-width: 2px;
-		$line-color: $color-green;
-		
-		--branch-padding-left: 2rem;
-		--line-top: .6rem;
-		--btn-width: 1rem;
-		
-		&::before {
-			position: absolute;
-			top: 0;
-			left: 1.5rem;
-			
-			display: block;
-			height: 1.5rem;
-			border-left: var(--line-width) solid $line-color;
-			content: "";
-		}
-		
-		.branch {
-			position: relative;
-			padding-left: var(--branch-padding-left);
-			
-			background-position: 0 0;
-			background-size: 1rem;
-			background-repeat: no-repeat;
-			
-			
-			::before,
-			::after,
-			&::after {
-				display: block;
-				content: "";
-			}
-			
-			&::after {
-				position: absolute;
-				top: calc(.5rem - var(--line-width) / 2);
-				left: calc(var(--btn-width) / 2 - var(--line-width) / 2);
-				display: block;
-				width: calc(var(--branch-padding-left) / 1.5);
-				height: calc(100% + .5rem);
-				border-left: 2px solid $color-green;
-				border-top: 2px solid $color-green;
-				content: "";
-				box-sizing: border-box;
-			}
-			
-			
-			&:last-child {
-				&::after {
-					border-left: none;
-					box-sizing: border-box;
-				}
-			}
-			
-			
-			&__name {
-				background-position: 0 0;
-				background-size: 1rem;
-				background-repeat: no-repeat;
-				background-image: url('/softline/img/icons/file.png');
-				padding-left: 1.2rem;
-			}
-			
-			
-			&__btn {
-				position: absolute;
-				top: .5rem;
-				left: 0;
-				
-				width: 1rem;
-				height: 1rem;
-				
-				line-height: 1;
-				border: 1px solid black;
-				border-radius: 3px;
-				padding: 0;
-				margin: 0;
-				
-				background-image: url('/softline/img/icons/plus.png');
-				background-size: 70%;
-				background-repeat: no-repeat;
-				background-position: center;
-				
-				cursor: pointer;
-				transform: translateY(-50%);
-				z-index: 5;
-				
-				&.opened {
-					background-image: url('/softline/img/icons/minus.png');
-				}
-			}
-			
-			&__subtree {
-				position: relative;
-				margin-top: 1rem;
-				
-				&::after {
-					position: absolute;
-					top: -1rem;
-					left: calc(.5rem - var(--line-width) / 2);
-					display: block;
-					width: 1rem;
-					height: 1.5rem;
-					border-left: 2px solid $color-green;
-					content: "";
-					box-sizing: border-box;
-				}
-			}
-			
-			+ .branch {
-				margin-top: .5rem;
-			}
-			
-			&--file {
-				.branch__name {
-					background-image: url('/softline/img/icons/file.png');
-				}
-			}
-			
-			&--folder {
-				.branch__name {
-					background-image: url('/softline/img/icons/folder-closed.png');
-				}
-				
-				&.opened {
-					.branch__name {
-						background-image: url('/softline/img/icons/folder-opened.png');
-					}
-				}
-			}
-		}
-	}
+
 </style>
--- src/views/Tree.vue
+++ src/views/Tree.vue
@@ -3,88 +3,221 @@
 		<h2>
 			Бинарная структура
 		</h2>
-		
-		<div class="tree">
+
+		<ul class="tree">
 			<Branch v-for="branch in tree" :key="branch.id" :info="branch" />
-		</div>
+		</ul>
 	</section>
 </template>
 
 <script>
-	import Branch from '@/components/Branch'
-	export default {
-		name: 'tree',
-		components: {
-			Branch
-		},
-		data() {
-			return {
-				tree: [
-					{
-						id: 1,
-						type: 'folder',
-						// name: 'Folder 1',
-						children: [
-							{
-								id: 10,
-								type: 'file',
-								name: 'Item 1.1',
-							}
-						]
-					},
-					{
-						id: 2,
-						type: 'folder',
-						name: 'Folder 2',
-						children: [
-							{
-								id: 20,
-								type: 'folder',
-								name: 'Subfolder 2.1',
-								children: [
-									{
-										id: 201,
-										type: 'file',
-										name: 'File 2.1.1',
-									}, 
-									{
-										id: 202,
-										type: 'file',
-										name: 'File 2.1.2',
-									}
-								]
-							}, 
-							{
-									id: 21,
+import Branch from '@/components/Branch'
+export default {
+	name: 'tree',
+	components: {
+		Branch
+	},
+	data() {
+		return {
+			tree: [
+				{
+					id: 1,
+					type: 'folder',
+					// name: 'Folder 1',
+					children: [
+						{
+							id: 10,
+							type: 'file',
+							name: 'Item 1.1',
+						}
+					]
+				},
+				{
+					id: 2,
+					type: 'folder',
+					name: 'Folder 2',
+					children: [
+						{
+							id: 20,
+							type: 'folder',
+							name: 'Subfolder 2.1',
+							children: [
+								{
+									id: 201,
 									type: 'file',
-									name: 'File 2.2',
-							}
-						]
-					},
-					{
-						id: 3,
-						type: 'folder',
-						name: 'Folder 3 (closed at start)',
-						children: []
-					},
-					{
-						id: 4,
-						type: 'file',
-						name: 'File 4',
-					}
-				]
-			}
+									name: 'File 2.1.1',
+								},
+								{
+									id: 202,
+									type: 'file',
+									name: 'File 2.1.2',
+								}
+							]
+						},
+						{
+							id: 21,
+							type: 'file',
+							name: 'File 2.2',
+						}
+					]
+				},
+				{
+					id: 3,
+					type: 'folder',
+					name: 'Folder 3 (closed at start)',
+					children: []
+				},
+				{
+					id: 4,
+					type: 'file',
+					name: 'File 4',
+				}
+			]
 		}
 	}
+}
 </script>
 
 <style lang="scss">
-	.tree {
-		--padding-left: 1rem;
-		--padding-top: 1rem;
-		
-		position: relative;
-		padding-top: var(--padding-top);
-		padding-left: var(--padding-left);
+.tree {
+	--line-width: 2px;
+	$line-color: $color-green;
+
+	--branch-padding-left: 2rem;
+	--btn-width: 1rem;
+
+	--padding-left: 1rem;
+	--padding-top: 1rem;
+
+	position: relative;
+	padding-top: var(--padding-top);
+	padding-left: var(--padding-left);
+	list-style-type: none;
+	margin: 0;
+
+	&::before {
+		position: absolute;
+		top: 0;
+		left: 1.5rem;
+
+		display: block;
+		height: 1.5rem;
+		border-left: var(--line-width) solid $line-color;
+		content: "";
 	}
+
+	&__branch {
+		position: relative;
+		padding-left: var(--branch-padding-left);
+
+		background-position: 0 0;
+		background-size: 1rem;
+		background-repeat: no-repeat;
+
+		::before,
+		::after,
+		&::after {
+			display: block;
+			content: "";
+		}
+
+		&::after {
+			position: absolute;
+			top: calc(.5rem - var(--line-width) / 2);
+			left: calc(var(--btn-width) / 2 - var(--line-width) / 2);
+			display: block;
+			width: calc(var(--branch-padding-left) / 1.5);
+			height: calc(100% + .5rem);
+			border-left: 2px solid $line-color;
+			border-top: 2px solid $line-color;
+			content: "";
+			box-sizing: border-box;
+		}
+
+		&:last-child {
+			&::after {
+				border-left: none;
+				box-sizing: border-box;
+			}
+		}
+
+		&:not(:first-child) {
+			margin-top: .5rem;
+		}
+
+		&--file {
+			.branch__name {
+				background-image: url('/img/icons/file.png');
+			}
+		}
+
+		&--folder {
+			> .tree__branch-name {
+				background-image: url('/img/icons/folder-closed.png');
+			}
+
+			&.opened {
+				> .tree__branch-name {
+					background-image: url('/img/icons/folder-opened.png');
+				}
+			}
+		}
+	}
+
+	&__btn {
+		position: absolute;
+		top: .5rem;
+		left: 0;
+
+		width: 1rem;
+		height: 1rem;
+
+		line-height: 1;
+		border: 1px solid black;
+		border-radius: 3px;
+		padding: 0;
+		margin: 0;
+
+		background-image: url('/img/icons/plus.png');
+		background-size: 70%;
+		background-repeat: no-repeat;
+		background-position: center;
+
+		cursor: pointer;
+		transform: translateY(-50%);
+		z-index: 5;
+
+		&.opened {
+			background-image: url('/img/icons/minus.png');
+		}
+	}
+
+	&__branch-name {
+		display: inline-block;
+		background-position: 0 0;
+		background-size: 1rem;
+		background-repeat: no-repeat;
+		background-image: url('/img/icons/file.png');
+		padding-left: 1.2rem;
+	}
+
+	&__subtree {
+		position: relative;
+		margin-top: 1rem;
+		padding-left: 0;
+		list-style-type: none;
+
+		&::after {
+			position: absolute;
+			top: -1rem;
+			left: calc(.5rem - var(--line-width) / 2);
+			display: block;
+			width: 1rem;
+			height: 1.5rem;
+			border-left: 2px solid $line-color;
+			content: "";
+			box-sizing: border-box;
+		}
+	}
+}
 </style>
