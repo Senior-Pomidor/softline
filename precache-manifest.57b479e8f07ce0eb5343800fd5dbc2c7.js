d186e43e66c38a19b31eccb336f261ef7b508158
Senior-Pomidor <bobylev.vladi@gmail.com>
2022-07-27 03:04:52 +0300

    build

--- precache-manifest.57b479e8f07ce0eb5343800fd5dbc2c7.js
+++ precache-manifest.57b479e8f07ce0eb5343800fd5dbc2c7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "287a673474534314b9c3",
+    "revision": "a4c2c008213c80ed024a",
     "url": "/softline/css/app.2298ed5c.css"
   },
   {
-    "revision": "43fa218f62ae304e7d6a",
-    "url": "/softline/css/chunk-d27bb0a8.087b1362.css"
+    "revision": "2acfb1501a934f1c48f1",
+    "url": "/softline/css/chunk-c2d2d6b8.087b1362.css"
   },
   {
-    "revision": "34e46265672912f7d5f31054f83c0c79",
+    "revision": "b63b22e934868850b111dd70013342a9",
     "url": "/softline/index.html"
   },
   {
-    "revision": "287a673474534314b9c3",
-    "url": "/softline/js/app.e378feaa.js"
+    "revision": "a4c2c008213c80ed024a",
+    "url": "/softline/js/app.81a17f34.js"
   },
   {
-    "revision": "43fa218f62ae304e7d6a",
-    "url": "/softline/js/chunk-d27bb0a8.44169366.js"
+    "revision": "2acfb1501a934f1c48f1",
+    "url": "/softline/js/chunk-c2d2d6b8.f94148af.js"
   },
   {
     "revision": "0a987fc6f4463400acb2",
